c28878303c73d280c3e5a9c5ed2d4a754151ea05
Simon L'Hérault <6261485@cegepoutaouais.qc.ca>
2025-09-25 16:59:42 -0400

    save 1

---
+++ code with me.py
@@ -0,0 +1,55 @@
+#Zigomar
+import random
+
+
+'''
+#pseudocode 
+C
+clés_mystérieuses
+boules_brillantes
+squelettes_miniatures
+#nouveau -->
+clés_mystérieuses
+squelettes_géant
+zombie_géant
+zombie_miniatures
+araignée_géant
+araignée_miniatures
+boules_crystale
+boules_d'ombre
+boules_d'énergie 
+white_monster
+black_monster
+green_monster
+
+if len(sac){>= 14} # if sac #rangement >=15 objet max sinon random.shuffle (1,2)
+1. retire tous objet/nombre "b"
+2. demande quel objet retirer
+
+    
+fin chasse_au_trésor
+random.shuffle(sac) print ("mélange aléatoire des objet du sac")
+random.choice(sac)  
+if random.choice(sac) == random.shuffle(sac)
+return print("Zigomar a deviner votre objet et vous remercie")
+else print("GRRRRR")
+sac = [ "potions_scintillantes","clés_mystérieuses","boules_brillantes","squelettes_miniatures"]
+
+list.sort(sac)
+'''
+
+def sac(item):
+    """
+
+    """
+
+
+if __name__ == "__main__":
+
+    item = ["potions_scintillantes", "clés_mystérieuses", "boules_brillantes", "squelettes miniatures"]
+    item_possible = ["squelettes_miniatures", "potions_scintillantes", "clés_mystérieuses", "boules_brillantes", "clés_mystérieuses",
+                    "squelettes_géant", "zombie_géant", "zombie_miniatures", "araignée_géant", "araignée_miniatures", "boules_crystale",
+                    "boules_d'ombre", "boules_d'énergie","white_monster","black_monster", "green_monster"]
+
+    input("Avez vous trouver un nouvel item ? ")
+
